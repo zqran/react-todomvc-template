090a8c5bc2e11b70530bede28d77d2de4a81748e
zqran <uuxnet@163.com>
2018-10-27 21:19:47 +0800

    init react component and style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* 
+  htmlFor
+  autoFocus
+  defaultChecked
+  defaultValue
+*/
+
+import React, { Component } from 'react'
+
+// 导入todos样式
+import './css/todos-base.css'
+import './css/todos-index.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input
+            className="new-todo"
+            placeholder="What needs to be done?"
+            autoFocus
+          />
         </header>
-      </div>
-    );
+
+        <section className="main">
+          <input id="toggle-all" className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <ul className="todo-list">
+            {/* editing */}
+            <li className="completed">
+              <div className="view">
+                <input className="toggle" type="checkbox" defaultChecked />
+                <label>Taste JavaScript</label>
+                <button className="destroy" />
+              </div>
+              <input
+                className="edit"
+                defaultValue="Create a TodoMVC template"
+              />
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Buy a unicorn</label>
+                <button className="destroy" />
+              </div>
+              <input className="edit" defaultValue="Rule the web" />
+            </li>
+          </ul>
+        </section>
+
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>0</strong> item left
+          </span>
+          <ul className="filters">
+            <li>
+              <a className="selected" href="#/">
+                All
+              </a>
+            </li>
+            <li>
+              <a href="#/active">Active</a>
+            </li>
+            <li>
+              <a href="#/completed">Completed</a>
+            </li>
+          </ul>
+          <button className="clear-completed">Clear completed</button>
+        </footer>
+      </section>
+    )
   }
 }
 
-export default App;
+export default App
